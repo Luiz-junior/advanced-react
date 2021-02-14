daad7ee28c3b03bf281657ecb604ee3c932fb498
Luiz <junior.rodrigues0222@gmail.com>
2021-02-14 11:16:48 -0300

    created GlobalStyles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Main from 'components/main'
 
-export default function Home({ title = 'React Avançado' }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
+export default function Home() {
+  return <Main />
 }
